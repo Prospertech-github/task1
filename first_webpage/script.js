5936017e1781ff2521b6575e488d1191b38a73b7
Prosper Ikechukwu <mrhendrix62@gmail.com>
2020-03-27 00:07:35 +0100

    the modified script.js file was used to carry out the javascript task

---
+++ first_webpage/script.js
@@ -0,0 +1,9 @@
+var name = "Prosper Ikechukwu" ;
+var courses = ["HTML", "CSS", "Javascript"] ;
+
+for (i = 0; i <= 200; i++) {
+    if (i % 2 != 0) {
+        continue;
+    }
+} ;
+console.log (i) ;
